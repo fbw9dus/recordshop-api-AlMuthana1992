8c6c74143001d3d18d05c193a3d6e29dc389d3ee
https://github.com/AlMuthana1992/m.hasan.git <test@test.test>
2020-05-04 10:09:32 +0200

    ok

--- __tests__/sanitization.js
+++ __tests__/sanitization.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 const faker = require('faker')
 const validator = require('validator')
+
 let server;
 let token;
+
 describe('Sanitization', () => {
     test('email in db should be sanitized', async done => {
         ////// EMAIL
@@ -18,6 +20,7 @@ describe('Sanitization', () => {
             role: 'User'
         }
         const sanitizedEmail = validator.normalizeEmail(unsanitizedData.email)
+        
         const res = await request(app)
             .post(`/users`)
             .set('x-auth', `${token}`)
